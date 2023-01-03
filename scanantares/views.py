f1cc56b1b5867528fb2d94c10c4ac68655b734c5
fikrimurod <murodfikri98@gmail.com>
2023-01-03 16:40:27 +0700

    add function

--- scanantares/views.py
+++ scanantares/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from scanantares.models import *
 
 
 def home(req):
-    return render(req,'main.html')
+
+    scanner = Scanner.objects.all()
+    sortir = Sortir.objects.all()
+    total_sortir = Sortir.objects.count()
+
+    
+    context = {
+        'scanner':scanner,
+        'sortir':sortir,
+        'total_sort':total_sortir,
+    }
+
+
+    return render(req,'main.html',context)
